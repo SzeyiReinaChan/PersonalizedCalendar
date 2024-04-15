3f7afdfc4ba92d8aa08eeeb3e18d88b83f768f96
szeyic@gmail.com <szeyic@gmail.com>
2024-04-15 14:59:24 -0400

    added linearUCB.py

--- simulator.py
+++ simulator.py
@@ -1,6 +1,7 @@
 import numpy as np
 import random
 import copy
+from linearUCB import LinearUCB
 np.random.seed(0)
 random.seed(0)
 
@@ -23,7 +24,8 @@ reading_constrain = [0, 1, 2, 3, 4]
 number_of_slots = 5
 
 
-def preference_generator(calendar):
+# For generating the preference for each event in each slot.
+def preferenceGenerator(calendar):
     preference_count = 0
     for each in calendar.values():
         if each in relevant_events:
@@ -48,6 +50,53 @@ def featureListGenerator(input, num_range):
     return features_list
 
 
+def get_irrelevant_calendar():
+    st_irrelevant_calendar = [-1]*number_of_slots  # empty calendar
+    for each in irrelevant_events:
+        # allow 1 in max for each irrelevant event
+        num_each = np.random.randint(0, 2, size=1)[0]
+        for _ in range(num_each):
+            # randomly assign the position for the irrelevant events
+            probability = np.random.uniform(0, 1, number_of_slots)
+            probability_normalized = probability / probability.sum()
+            each_position = np.random.choice(
+                [0, 1, 2, 3, 4], p=probability_normalized)
+            print("each_position=", each_position)
+
+            if st_irrelevant_calendar[each_position] == -1:
+                st_irrelevant_calendar[each_position] = each
+            else:
+                # if there is already an event, skip
+                print("Skipped placement")
+                continue
+        print("st_irrelevant_calendar=", st_irrelevant_calendar)
+    return st_irrelevant_calendar
+
+
+def eligibleActionsGenerator(st_irrelevant_calendar):
+    slots_available = [i for i in range(
+        number_of_slots) if st_irrelevant_calendar[i] == -1]
+    print("slots_available=", slots_available)
+
+    possible_calendars = []
+    for assignment_out_slot in slots_available:
+        for reading_slot in slots_available:
+            if reading_slot == assignment_out_slot:
+                continue
+            if assignment_out_slot not in assignment_out_constrain:
+                continue
+            if reading_slot not in reading_constrain:
+                continue
+            new_calendar = copy.copy(st_irrelevant_calendar)
+            new_calendar[assignment_out_slot] = "assignment_out"
+            new_calendar[reading_slot] = "reading"
+            possible_calendars.append(new_calendar)
+    print("============Possible Actions============")
+    for i in possible_calendars:
+        print(i)
+    return slots_available, possible_calendars
+
+
 def simulation(rounds=2):
     # initialize_feature = featureListGenerator(
     #     calendar, [1, number_of_slots + 1])
@@ -55,7 +104,7 @@ def simulation(rounds=2):
     # print("initialize_feature= ", initialize_feature)
 
     # simulate preference
-    # preference = preference_generator(calendar)
+    # preference = preferenceGenerator(calendar)
     preference = np.random.uniform(
         0, 1, len(relevant_events)*number_of_slots)
     print("============Preference============")
@@ -63,127 +112,168 @@ def simulation(rounds=2):
 
     # assert (len(preference) == len(initialize_feature))
 
+    # initialize the algorithm
+    linear_ucb = LinearUCB(relevant_events, number_of_slots)
+
     # initialize reward
-    total_reward = 0.0
-    # # 10 rounds of simulation
+    ucb_total_reward = 0.0
+
+    # Rounds of simulations
     for t in range(rounds):
-        print("\n============ Round ", t, "============")
+        print("\n============ Round", t, "============")
+        st_irrelevant_calendar = get_irrelevant_calendar()
+        slots_available, possible_calendars = eligibleActionsGenerator(
+            st_irrelevant_calendar)
+
+        # LinearUCB simulation===================================
+        # generate the feature factors for each possible calendar
+        feature_factors = []
+        for each_possible_calendar in possible_calendars:
+            feature_factors.append(featureListGenerator(each_possible_calendar, [number_of_slots]))
+
+        ucb_chosen_action_at = linear_ucb.actionSelection(possible_calendars, feature_factors)
+        print("action=", ucb_chosen_action_at)
+
+        # get the reward - reward of the true human preference
+        chosen_action_feature_list = featureListGenerator(ucb_chosen_action_at, [number_of_slots])
+        ucb_human_reward = np.dot(preference, chosen_action_feature_list)
+        print("human reward=", ucb_human_reward)
+
+        # update the reward based on the human rating + random simulated noise
+        ucb_noisy_reward = ucb_human_reward + np.random.normal(loc=0.0, scale=0.1)
+        print("noisy reward=", ucb_noisy_reward)
+
+        # update the algorithm based on the reward
+        linear_ucb.updateByRating(ucb_noisy_reward, feature_factors)
+        
+        # for evaluation purpose
+        ucb_total_reward += ucb_noisy_reward
+        print("\ntotal_reward=", ucb_total_reward)
+
+
 
         # 1. Get st=irrelevant calendar
-        st_irrelevant_calendar = [-1]*number_of_slots  # empty calendar
-        for each in irrelevant_events:
-            # allow 1 in max for each irrelevant event
-            num_each = np.random.randint(0, 2, size=1)[0]
-            for _ in range(num_each):
-                # randomly assign the position for the irrelevant events
-                probability = np.random.uniform(0, 1, number_of_slots)
-                probability_normalized = probability / probability.sum()
-                each_position = np.random.choice(
-                    [0, 1, 2, 3, 4], p=probability_normalized)
-                print("each_position=", each_position)
+        # st_irrelevant_calendar = [-1]*number_of_slots  # empty calendar
+        # for each in irrelevant_events:
+        #     # allow 1 in max for each irrelevant event
+        #     num_each = np.random.randint(0, 2, size=1)[0]
+        #     for _ in range(num_each):
+        #         # randomly assign the position for the irrelevant events
+        #         probability = np.random.uniform(0, 1, number_of_slots)
+        #         probability_normalized = probability / probability.sum()
+        #         each_position = np.random.choice(
+        #             [0, 1, 2, 3, 4], p=probability_normalized)
+        #         print("each_position=", each_position)
 
-                if st_irrelevant_calendar[each_position] == -1:
-                    st_irrelevant_calendar[each_position] = each
-                else:
-                    # if there is already an event, skip
-                    continue
-            print("st_irrelevant_calendar=", st_irrelevant_calendar)
+        #         if st_irrelevant_calendar[each_position] == -1:
+        #             st_irrelevant_calendar[each_position] = each
+        #         else:
+        #             # if there is already an event, skip
+        #             continue
+        #     print("st_irrelevant_calendar=", st_irrelevant_calendar)
 
         # 2. Find all eligible actions
         # E.g., constraint: in this week, both assignment_out and reading have to be assigned.
         # assignment_out has to be slot 0,1,2. reading can be anytime.
-        slots_available = [i for i in range(
-            number_of_slots) if st_irrelevant_calendar[i] == -1]
-        print("slots_available=", slots_available)
+        # slots_available = [i for i in range(
+        #     number_of_slots) if st_irrelevant_calendar[i] == -1]
+        # print("slots_available=", slots_available)
 
-        possible_calendars = []
-        for assignment_out_slot in slots_available:
-            for reading_slot in slots_available:
-                if reading_slot == assignment_out_slot:
-                    continue
-                if assignment_out_slot not in assignment_out_constrain:
-                    continue
-                if reading_slot not in reading_constrain:
-                    continue
-                new_calendar = copy.copy(st_irrelevant_calendar)
-                new_calendar[assignment_out_slot] = "assignment_out"
-                new_calendar[reading_slot] = "reading"
-                possible_calendars.append(new_calendar)
-        print("============Possible Actions============")
-        for i in possible_calendars:
-            print(i)
+        # possible_calendars = []
+        # for assignment_out_slot in slots_available:
+        #     for reading_slot in slots_available:
+        #         if reading_slot == assignment_out_slot:
+        #             continue
+        #         if assignment_out_slot not in assignment_out_constrain:
+        #             continue
+        #         if reading_slot not in reading_constrain:
+        #             continue
+        #         new_calendar = copy.copy(st_irrelevant_calendar)
+        #         new_calendar[assignment_out_slot] = "assignment_out"
+        #         new_calendar[reading_slot] = "reading"
+        #         possible_calendars.append(new_calendar)
+        # print("============Possible Actions============")
+        # for i in possible_calendars:
+        #     print(i)
+
+        # # 3. Algorithm selection
+        # if selection == "linearUCB":
+        #     action, reward = linearUCB(possible_calendars, preference)
+
+        # total_reward += reward
+        # print("\ntotal_reward=", total_reward)
 
         # 3. Algorithm chooses which action to run
         # For simulation purpose, let's randomly choose one for now.
         # Algorithm: input: human_rating / human_correction from t-1. output: action_chosen at t.
-        print("============Actions Chosen============")
-        probability = [1/len(possible_calendars)] * len(possible_calendars)
-        action_index_chosen = np.random.choice(
-            range(len(possible_calendars)), p=probability)
-        action_chosen = possible_calendars[action_index_chosen]
-        print("action_index_chosen=", action_index_chosen,
-              ", action_chosen=", action_chosen)
+        # print("============Actions Chosen============")
+        # probability = [1/len(possible_calendars)] * len(possible_calendars)
+        # action_index_chosen = np.random.choice(
+        #     range(len(possible_calendars)), p=probability)
+        # action_chosen = possible_calendars[action_index_chosen]
+        # print("action_index_chosen=", action_index_chosen,
+        #       ", action_chosen=", action_chosen)
 
-        # 4. Get reward
-        features_list = []
-        for each in relevant_events:
-            for i in range(number_of_slots):
-                # print("each, i: ", each, i)
-                if action_chosen[i] == each:
-                    features_list.append(1)
-                else:
-                    features_list.append(0)
-        print("============Get Reward============")
-        print("features_list=", features_list)
-        # Reward is for evaluating the algorithm
-        reward = np.dot(preference, features_list)
-        print("reward=", reward)
-        total_reward += reward
+        # # 4. Get reward
+        # features_list = []
+        # for each in relevant_events:
+        #     for i in range(number_of_slots):
+        #         # print("each, i: ", each, i)
+        #         if action_chosen[i] == each:
+        #             features_list.append(1)
+        #         else:
+        #             features_list.append(0)
+        # print("============Get Reward============")
+        # print("features_list=", features_list)
+        # # Reward is for evaluating the algorithm
+        # reward = np.dot(preference, features_list)
+        # print("reward=", reward)
+        # total_reward += reward
 
         # 5. Human feedback I
         # Getting human feedback from likert scale or thumbs up/down
         # For now, let's assume the human feedback is the same as the reward with some noise
         # TODO: Add algorithm to learn from human feedback here (estimate preference)
         # Paper: A Contextual-Bandit Approach to Personalized News Article Recommendation
-        noise = np.random.normal(loc=0.0, scale=0.1)
-        human_rating = reward + noise
+        # noise = np.random.normal(loc=0.0, scale=0.1)
+        # human_rating = reward + noise
         # TODO: change to integer (likert scale)
-        print("(Noisy) human_rating=", human_rating)
+        # print("(Noisy) human_rating=", human_rating)
 
         # 6. Human feedback II
         # Getting human feedback from human correction
         # TODO: Add algorithm to learn from human feedback here (estimate preference)
         # Paper: Coactive Learning
-        calendar_highest_reward = None
-        reward_highest = -np.Inf
-        for eligible_calendar in possible_calendars:
-            # update the feature list because the calendar has been updated
-            updated_feature_list = featureListGenerator(
-                eligible_calendar, [number_of_slots])
-            eligible_calendar_reward = np.dot(preference, updated_feature_list)
-            print("eligible_calendar=", eligible_calendar, ", features_list=",
-                  updated_feature_list, ", reward=", eligible_calendar_reward)
+        # calendar_highest_reward = None
+        # reward_highest = -np.Inf
+        # for eligible_calendar in possible_calendars:
+        #     # update the feature list because the calendar has been updated
+        #     updated_feature_list = featureListGenerator(
+        #         eligible_calendar, [number_of_slots])
+        #     eligible_calendar_reward = np.dot(preference, updated_feature_list)
+        #     print("eligible_calendar=", eligible_calendar, ", features_list=",
+        #           updated_feature_list, ", reward=", eligible_calendar_reward)
 
-            # find the calendar with the highest reward
-            if eligible_calendar_reward > reward_highest:
-                reward_highest = eligible_calendar_reward
-                calendar_highest_reward = eligible_calendar
+        #     # find the calendar with the highest reward
+        #     if eligible_calendar_reward > reward_highest:
+        #         reward_highest = eligible_calendar_reward
+        #         calendar_highest_reward = eligible_calendar
 
         # print("calendar_highest_reward=", calendar_highest_reward)
 
         # assume the human correction is the optimal calendar
-        human_correction = calendar_highest_reward
-        print("(Noieless for now) human_correction=", human_correction)
+        # human_correction = calendar_highest_reward
+        # print("(Noieless for now) human_correction=", human_correction)
 
-    # Evaluation of the algorithm by the total reward
-    # The higher total reward, the better
-    print("\n\ntotal_reward=", total_reward)
+        # Evaluation of the algorithm by the total reward
+        # The higher total reward, the better
+        # print("\n\ntotal_reward=", total_reward)
 
+        # def testCase1():
+        #     calendar1 = {1: "gym", 2: "assignment_out",
+        #                  3: "reading", 4: -1, 5: "class"}
+        #     simulation(calendar1)
 
-# def testCase1():
-#     calendar1 = {1: "gym", 2: "assignment_out",
-#                  3: "reading", 4: -1, 5: "class"}
-#     simulation(calendar1)
 
 def main():
     # TODO: offline procedure to extract information
