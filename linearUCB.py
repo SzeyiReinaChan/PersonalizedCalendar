aade425bb6eeeb09a889c54ea1492f47b372d51f
szeyic@gmail.com <szeyic@gmail.com>
2024-04-16 18:20:21 -0400

    fixed bug

--- linearUCB.py
+++ linearUCB.py
@@ -46,12 +46,13 @@ class LinearUCB:
                 self.b_a_s.append(np.zeros(d))
 
             # calculate the machine estimated human preference
-            assert np.shape(self.A_a_s[action_index]) == (d, d)
-            assert np.shape(self.b_a_s[action_index]) == (d,)
+            extracted_action_key = None
             for key, value in self.explored_actions.items():
                 if value == A_list[action_index]:
                     extracted_action_key = key
                     break
+            assert np.shape(self.A_a_s[extracted_action_key]) == (d, d)
+            assert np.shape(self.b_a_s[extracted_action_key]) == (d,)
             # print("A_a_s=", self.A_a_s[extracted_action_key],
             #       "b_a_s=", self.b_a_s[extracted_action_key])
             theta_hat.append(np.dot(np.linalg.inv(
@@ -61,14 +62,19 @@ class LinearUCB:
             current_feature_factors = np.array(
                 feature_factors[action_index])
             assert np.shape(np.dot(np.linalg.inv(
-                self.A_a_s[action_index]), feature_factors[action_index])) == (d,)
+                self.A_a_s[extracted_action_key]), feature_factors[action_index])) == (d,)
             assert np.shape(np.dot(current_feature_factors.T, np.dot(np.linalg.inv(
-                self.A_a_s[action_index]), feature_factors[action_index]))) == ()
+                self.A_a_s[extracted_action_key]), feature_factors[action_index]))) == ()
+            # print("\nA_list = ", A_list[action_index],
+            #       ", extracted_action_key=", extracted_action_key)
             # print("current_feature_factors=", current_feature_factors)
-            # print("theta_hat= ", theta_hat[action_index], "ucb_term 1=", np.dot(theta_hat[action_index].T, feature_factors[action_index]), "ucb term2= ", alpha*np.sqrt(np.dot(
-            # current_feature_factors.T, np.dot(np.linalg.inv(self.A_a_s[action_index]), feature_factors[action_index]))), "A_list = ", A_list[action_index])
+            # print("theta_hat= ", theta_hat[action_index])
+            # print("ucb1=", np.dot(
+            #     theta_hat[action_index].T, feature_factors[action_index]))
+            # print("ucb2= ", alpha*np.sqrt(np.dot(
+            #     current_feature_factors.T, np.dot(np.linalg.inv(self.A_a_s[extracted_action_key]), feature_factors[action_index]))))
             p_t_a.append(np.dot(theta_hat[action_index].T, feature_factors[action_index]
-                                ) + alpha*np.sqrt(np.dot(current_feature_factors.T, np.dot(np.linalg.inv(self.A_a_s[action_index]), feature_factors[action_index]))))
+                                ) + alpha*np.sqrt(np.dot(current_feature_factors.T, np.dot(np.linalg.inv(self.A_a_s[extracted_action_key]), feature_factors[action_index]))))
 
         # choose the action with the highest UCB
         self.action_index_chosen = np.argmax(p_t_a)
@@ -101,7 +107,7 @@ class LinearUCB:
         feature_chosen_reshaped = np.array(feature_chosen).reshape(1, d)
         assert np.shape(feature_chosen *
                         np.array(feature_chosen_reshaped).T) == (d, d)
-        # print(self.action)
+        # print("Update ", self.action)
         # print("old A_a_s=", self.A_a_s[extracted_action_key])
         self.A_a_s[extracted_action_key] += feature_chosen * \
             np.array(feature_chosen_reshaped).T
--- simulator.py
+++ simulator.py
@@ -13,8 +13,8 @@ random.seed(0)
 # D_duration = [i for i in range(24*60+1)]
 # D_periods = {"morning": 0, "afternoon": 1, "evening": 2}
 
-# irrelevant_events = ["gym", "class"]
-irrelevant_events = []
+irrelevant_events = ["gym", "class"]
+# irrelevant_events = []
 relevant_events = ["assignment_out", "reading"]
 # relevant_events = ["reading"]
 events = irrelevant_events + relevant_events
@@ -38,7 +38,7 @@ def preferenceGenerator(calendar):
     for each in calendar.values():
         if each in relevant_events:
             preference_count += 1
-    print("preference_count=", preference_count)
+    # print("preference_count=", preference_count)
 
     # randomly generated θ(preference) for evaluation, e.g., θ is uniformly random
     preference = np.random.uniform(0, 1, preference_count*number_of_slots)
@@ -50,7 +50,7 @@ def featureListGenerator(input, num_range):
     features_list = []
     for each in relevant_events:
         for i in range(*num_range):
-            # print("each, i: ", each, i)
+            # #print("each, i: ", each, i)
             if input[i] == each:
                 features_list.append(1)
             else:
@@ -69,22 +69,22 @@ def get_irrelevant_calendar():
             probability_normalized = probability / probability.sum()
             each_position = np.random.choice(
                 [0, 1, 2, 3, 4], p=probability_normalized)
-            print("each_position=", each_position)
+            # print("each_position=", each_position)
 
             if st_irrelevant_calendar[each_position] == -1:
                 st_irrelevant_calendar[each_position] = each
             else:
                 # if there is already an event, skip
-                print("Skipped placement")
+                # print("Skipped placement")
                 continue
-        print("st_irrelevant_calendar=", st_irrelevant_calendar)
+        # print("st_irrelevant_calendar=", st_irrelevant_calendar)
     return st_irrelevant_calendar
 
 
 def eligibleActionsGenerator(st_irrelevant_calendar):
     slots_available = [i for i in range(
         number_of_slots) if st_irrelevant_calendar[i] == -1]
-    print("slots_available=", slots_available)  # [-1, 1, -1, -1, -1]
+    # print("slots_available=", slots_available)
 
     possible_calendars = []
 
@@ -115,8 +115,8 @@ def eligibleActionsGenerator(st_irrelevant_calendar):
     #     for slot_index in range(len(slots_available)):  # index: 0, 1
     #         name = relevant_events[relevant_event_index]  # reading
     #         constrain_name = name + "_constrain"  # reading_constrain
-    #         print(constrain_name, "=", globals()[constrain_name])
-    #         print(slot_index)
+    #         #print(constrain_name, "=", globals()[constrain_name])
+    #         #print(slot_index)
     #         if slots_available[slot_index] != -1:
     #             continue
 
@@ -127,7 +127,7 @@ def eligibleActionsGenerator(st_irrelevant_calendar):
     #         constrain_name = each + "_constrain"
     #         if slot_index in globals()[constrain_name]:
 
-    #     print("slot_available=", slot_for_assignment)
+    #     #print("slot_available=", slot_for_assignment)
     #     for relevant_event_index in range(len(relevant_events)):
     #         name = relevant_events[relevant_event_index]
     #         constrain_name = name + "_constrain"
@@ -136,9 +136,9 @@ def eligibleActionsGenerator(st_irrelevant_calendar):
     #             new_calendar[slot_index] = name
     #             possible_calendars.append(new_calendar)
 
-    print("============Possible Actions============")
-    for i in possible_calendars:
-        print(i)
+    # print("============Possible Actions============")
+    # for i in possible_calendars:
+        # print(i)
     return slots_available, possible_calendars
 
 
@@ -155,14 +155,14 @@ def plot_data(dataset_1, dataset_2, title, plot_filename):
     plt.close()
 
 
-def simulation(rounds=1000):
+def simulation(rounds=int(1e5)):
     # simulate preference
     # preference = preferenceGenerator(calendar)
     # preference = np.random.uniform(
     #     0, 1, len(relevant_events)*number_of_slots)
     preference = [0, 1, 0, 0, 0, 0, 0, 0, 1, 0]
-    print("============Preference============")
-    print("preference=", preference)
+    # print("============Preference============")
+    # print("preference=", preference)
 
     # initialize the algorithm
     linear_ucb = LinearUCB(relevant_events, number_of_slots)
@@ -172,7 +172,7 @@ def simulation(rounds=1000):
 
     # Rounds of simulations
     for t in range(rounds):
-        print("\n============ Round", t+1, "============")
+        # print("\n============ Round", t+1, "============")
         st_irrelevant_calendar = get_irrelevant_calendar()
         slots_available, possible_calendars = eligibleActionsGenerator(
             st_irrelevant_calendar)
@@ -186,31 +186,42 @@ def simulation(rounds=1000):
 
         ucb_chosen_action_at = linear_ucb.actionSelection(
             possible_calendars, feature_factors)
-        print("ucb action chosen=", ucb_chosen_action_at)
+        # print("ucb action chosen=", ucb_chosen_action_at)
 
         # get the reward for the best possible calendar
-        all_possible_reward = []
+        # all_possible_reward = []
+        # for each_feature in feature_factors:
+        #     possible_reward = np.dot(preference, np.array(each_feature))
+        #     all_possible_reward.append(possible_reward)
+        # best_possible_reward = max(all_possible_reward)
+        # #print("ucb best possible reward=", best_possible_reward)
+
+        best_reward = -np.inf
+        best_reward_feature = None
         for each_feature in feature_factors:
             possible_reward = np.dot(preference, np.array(each_feature))
-            all_possible_reward.append(possible_reward)
-        best_possible_reward = max(all_possible_reward)
-        print("ucb best possible reward=", best_possible_reward)
+            if possible_reward > best_reward:
+                best_reward = possible_reward
+                best_reward_feature = each_feature
+        best_possible_reward = best_reward
+        # print("best_reward=", best_reward)
+        # print("best_reward_feature=", best_reward_feature)
 
         # get the reward - reward of the true human preference
         chosen_action_feature_list = featureListGenerator(
             ucb_chosen_action_at, [number_of_slots])
         ucb_human_reward = np.dot(preference, chosen_action_feature_list)
-        print("ucb human reward=", ucb_human_reward)
+        # print("ucb human reward=", ucb_human_reward)
 
         # update the reward based on the human rating + random simulated noise
-        # ucb_noisy_reward = ucb_human_reward + \
-        #     np.random.normal(loc=0.0, scale=0.1)
-        ucb_noisy_reward = ucb_human_reward
-        print("ucb noisy reward=", ucb_noisy_reward)
+        ucb_noisy_reward = ucb_human_reward + \
+            np.random.normal(loc=0.0, scale=0.1)
+        # ucb_noisy_reward = ucb_human_reward
+        # print("ucb noisy reward=", ucb_noisy_reward)
 
         # getting the regression number: how far away from the best possible reward?
         regret_number = best_possible_reward-ucb_noisy_reward
-        print("ucb regression_number=", regret_number)
+        # print("ucb regression_number=", regret_number)
 
         # update the algorithm based on the reward
         linear_ucb.updateByRating(
@@ -218,9 +229,9 @@ def simulation(rounds=1000):
 
         # for evaluation purpose
         ucb_total_reward += ucb_noisy_reward
-        print("\nucb total_reward=", ucb_total_reward)
+        # print("\nucb total_reward=", ucb_total_reward)
         ucb_total_regret += regret_number
-        print("ucb total_regret=", ucb_total_regret)
+        # print("ucb total_regret=", ucb_total_regret)
 
         # store the reward for plotting
         ucb_reward_dataset.append(ucb_total_reward)
