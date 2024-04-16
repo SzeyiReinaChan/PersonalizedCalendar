9cf02cdb45e388fc157db6b542c96e9fab82019a
kaiqiz8 <kzhang49@binghamton.edu>
2024-04-16 19:27:38 -0400

    remove preferencePerceptron.py

--- preferencePerceptron.py
+++ preferencePerceptron.py
@@ -1,30 +1,30 @@
-import numpy as np
+# import numpy as np
 
-class PreferencePerceptron:
-    # def __init__(self, number_of_features):
-    #     self.w = np.zeros(number_of_features)
-    # def predict(self, features_list):
-    #     predictions = [np.dot(self.w, features) for features in features_list]
-    #     return np.argmax(predictions)
+# class PreferencePerceptron:
+#     # def __init__(self, number_of_features):
+#     #     self.w = np.zeros(number_of_features)
+#     # def predict(self, features_list):
+#     #     predictions = [np.dot(self.w, features) for features in features_list]
+#     #     return np.argmax(predictions)
     
-    # def update(self, chosen_features, true_features):
-    #     self.w += (true_features - chosen_features)
+#     # def update(self, chosen_features, true_features):
+#     #     self.w += (true_features - chosen_features)
 
-#version 2
-    def __init__(self, number_of_slots, number_of_features):
-        self.w = np.zeros(number_of_slots * number_of_features)
+# #version 2
+#     def __init__(self, number_of_slots, number_of_features):
+#         self.w = np.zeros(number_of_slots * number_of_features)
 
-    def phi(self, features_list, action):
-        #define the feature representation function phi
-        return np.array(features_list[action])
+#     def phi(self, features_list, action):
+#         #define the feature representation function phi
+#         return np.array(features_list[action])
     
-    def predict(self, features_list):
-        predictions = [np.dot(self.w, features) for features in features_list]
-        return np.argmax(predictions)
+#     def predict(self, features_list):
+#         predictions = [np.dot(self.w, features) for features in features_list]
+#         return np.argmax(predictions)
     
-    def update(self, features_list, chosen_features, true_features):
-        # self.w += (true_features - chosen_features)
-        self.w += self.phi(features_list, true_features) - self.phi(features_list, chosen_features)
+#     def update(self, features_list, chosen_features, true_features):
+#         # self.w += (true_features - chosen_features)
+#         self.w += self.phi(features_list, true_features) - self.phi(features_list, chosen_features)
 
     
 
